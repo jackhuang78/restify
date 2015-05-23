d3574a11001843a385d3fde8771ef149577e0352
Jack Huang <jack.huang78@gmail.com>
2015-05-23 00:27:28 -0700

    load table names from backend

--- src/flux/QueryPage.js
+++ src/flux/QueryPage.js
@@ -1,9 +1,10 @@
 var React = require('react');
+var $ = require('jquery');
 
 var TableSelectItem = React.createClass({
 	render: function() {
 		return (
-			<option>abc</option>
+			<option>{this.props.item}</option>
 		);
 	}
 });
@@ -12,18 +13,36 @@ var TableSelect = React.createClass({
 	render: function() {
 		return (
 			<select>
-				<TableSelectItem />
+				{
+					this.props.items.map(function(item, idx) { 
+						return (
+							<TableSelectItem item={item} /> 
+						);	
+					})
+				}
 			</select>
 		);
 	}
 });
 
 var QueryPage = React.createClass({
+	getInitialState: function() {
+		return {items: []};
+	},
+	componentDidMount: function() {
+		$.get('_collections', function(items, status) {
+			if(status !== 'success') {
+				alert('Failed to load tables');
+			} else {
+				this.setState({items: items});
+			}
+		}.bind(this));
+	},
 	render: function() {
 		return (
 			<div>
 				<h1>QueryPage</h1>
-				<TableSelect />
+				<TableSelect items={this.state.items} />
 			</div>
 			
 		);
