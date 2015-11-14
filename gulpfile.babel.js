54d36036fdde0776588fb7f299c706fb7087df95
Jack Huang <jackhuang@machinezone.com>
2015-11-13 18:15:18 -0800

    add lint task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,17 @@
 import gulp from 'gulp';
 import mocha from 'gulp-mocha';
 import babel from 'gulp-babel';
+import eslint from 'gulp-eslint';
 
 gulp.task('default', () => {
 	console.log('Hello Gulp!');
+});
+
+gulp.task('lint', () => {
+	return gulp.src(['src/**/*.js', 'test/**/*.js'])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
 });
 
 gulp.task('test', () => {
